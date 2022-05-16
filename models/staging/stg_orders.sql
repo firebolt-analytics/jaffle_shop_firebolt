c307f8e0b60cd14732a62844dd2b500fc9d26ef5
ericf-firebolt <eric@firebolt.io>
2022-05-16 11:00:47 -0400

    Cleaned up stg_orders.sql.

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -10,21 +10,21 @@
 
 
 WITH source AS (
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    SELECT * FROM {{ ref('raw_orders') }}
-    {% if is_incremental() %}
-       WHERE order_date > (SELECT CAST(MAX(order_date) AS DATE)-3 FROM {{ this }})
-    {% endif %}
+  {#-
+  Normally we would select from the table here, but we are using seeds to load
+  our data in this project
+  #}
+  SELECT * FROM {{ ref('raw_orders') }}
+  {% if is_incremental() %}
+     WHERE order_date > (SELECT CAST(MAX(order_date) AS DATE)-3 FROM {{ this }})
+  {% endif %}
 ),
 renamed AS (
-    SELECT
-        id AS order_id,
-        user_id AS customer_id,
-        order_date,
-        status
-    FROM source
+  SELECT
+      id AS order_id,
+      user_id AS customer_id,
+      order_date,
+      status
+  FROM source
 )
 SELECT * FROM renamed
