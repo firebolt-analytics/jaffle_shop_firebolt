89888bec20d355f2ca6ca8421a6b83f1fdf01dfb
ericf-firebolt <eric@firebolt.io>
2022-05-16 11:00:24 -0400

    Cleaned up stg_customers.sql.

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -9,23 +9,22 @@
 }}
 
 WITH source AS (
-    {#-
-    Normally we would select from the table here, but we are using seeds to load
-    our data in this project
-    #}
-    SELECT * FROM {{ source('s3', 'raw_customers') }}
-    {%- if is_incremental() %}
-
-      -- this filter will only be applied on an incremental run
-      WHERE id > (SELECT MAX(customer_id)-5 FROM {{ this }})
-    {%- endif -%}
+  {#-
+  Normally we would select from the table here, but we are using seeds to load
+  our data in this project
+  #}
+  SELECT * FROM {{ source('s3', 'raw_customers') }}
+  {%- if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    WHERE id > (SELECT MAX(customer_id)-5 FROM {{ this }})
+  {%- endif -%}
 ),
 renamed AS (
-    SELECT
-        id AS customer_id,
-        first_name,
-        last_name
-    FROM source
+  SELECT
+      id AS customer_id,
+      first_name,
+      last_name
+  FROM source
 )
 
 SELECT * FROM renamed
